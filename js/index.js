2428f90f37b565d0f8c729db57f3a47cfa28ced6
lucasvianav <oviana@outlook.com>
2020-11-03 20:35:21 -0300

    login.js finished(?)

--- js/index.js
+++ js/index.js
@@ -49,3 +49,22 @@ function closeModal(){
 String.prototype.title = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
+
+// CPF Validation
+class CPF {
+    constructor() {
+        "user_strict"; function r(r) {
+            for (var t = null, n = 0; 9 > n; ++n)
+                t += r.toString().charAt(n) * (10 - n); var i = t % 11; return i = 2 > i ? 0 : 11 - i;
+        } function t(r) {
+            for (var t = null, n = 0; 10 > n; ++n)
+                t += r.toString().charAt(n) * (11 - n); var i = t % 11; return i = 2 > i ? 0 : 11 - i;
+        } var n = false, i = true; 
+        
+        this.validate = function (o) {
+            for (var a = o.replace(/\D/g, ""), u = a.substring(0, 9), f = a.substring(9, 11), v = 0; 10 > v; v++)
+                if ("" + u + f == "" + v + v + v + v + v + v + v + v + v + v + v)
+                    return n; var c = r(u), e = t(u + "" + c); return f.toString() === c.toString() + e.toString() ? i : n;
+        };
+    }
+}
--- js/login.js
+++ js/login.js
@@ -1,84 +1,207 @@
-function initializeScripts(){
-    handleFormTabs()
-    handleForgotPwButton()
+// Initializes js when the HTML has finished loading
+$(function(){
+    // Adds event listeners to the forms' titles buttons
+    let switchers = document.querySelectorAll('.switcher')
+    switchers.forEach(item => item.addEventListener('click', swapTab))
+    
+    showLoginRecovery()
+    handleForgotPw()
 
-    document.querySelector('#signup-name').onkeydown = handleNameTyping
-    document.querySelector('#signup-birthday').onkeydown = formatBirthday
-    document.querySelector('#signup-cpf').onkeydown = formatCPF
-    // document.querySelector('#signup-rg').onkeydown = formatRG
-    document.querySelector('#signup-phoneNumber').onkeydown = formatPhoneNumber
-}
+    $('#signup-name').on('keydown',handleNameTyping)
+    $('#signup-birthday').on('keydown',formatBirthday)
+    $('#signup-cpf').on('keydown',formatCPF)
+    $('#signup-phoneNumber').on('keydown',formatPhoneNumber)
+})
 
 // Login --> Criar conta | Criar conta --> Login
 function swapTab(){
-    const switchers = document.querySelectorAll('.switcher')
-    switchers.forEach(item => item.parentElement.classList.remove('active')) // Hides every forms
-    this.parentElement.classList.add('active') // Shows the current forms
+    if(!$(this.parentElement).hasClass('active')){
+        if($(this).hasClass('switcher-login')){ showLoginRecovery() }
+        else{ showSignup() }
+        
+        $('.form-wrapper.active').removeClass('active')
+        $(this.parentElement).addClass('active')
+    }
 }
 
-// Adds event listeners to the forms' titles buttons
-function handleFormTabs(){
-    const switchers = document.querySelectorAll('.switcher')
-    switchers.forEach(item => {
-        item.addEventListener('click', swapTab)
-    })
+// Activates animation to show the login and password recovery (leftmost) 
+// forms and hide the signup (leftmost) ones
+function showLoginRecovery(){
+    $('.form-login').addClass('show-login')
+    $('.form-recovery').addClass('show-recovery')
+    $('.form-signup').addClass('hide-signup')
+    $('.form-full-signup').addClass('hide-full-signup')
+
+    setTimeout(function(){
+        $('.form-login').removeClass('show-login')
+        $('.form-recovery').removeClass('show-recovery')
+        $('.form-signup').removeClass('hide-signup')
+        $('.form-full-signup').removeClass('hide-full-signup')
+
+        $('.form-login').css({
+            'background-color': '#fff',
+            'transform': 'translate(35%, -20px) scaleY(1)'
+        })
+
+        $('.form-recovery').css({
+            'background-color': '#fff',
+            'transform': 'translate(35%, -20px) scaleY(1)'
+        })
+
+        $('.form-signup').css({
+            'background-color': '#d7e7f1',
+            'transform': 'translate(-40%, 10px) scaleY(.8)'
+        })
+
+        $('.form-full-signup').css({
+            'background-color': '#d7e7f1',
+            'transform': 'translate(-40%, 10px) scaleY(.7)'
+        })
+
+    }, 400)
 }
 
-// What happens when the "I forgot my password" is pressed
-function handleForgotPwButton(){
-    const forgotButton = document.getElementById("forgot-button")
+// Activates animation to show the signup (rightmost) forms and hide
+// the login and password recovery (leftmost) ones
+function showSignup(){
+    $('.form-signup').addClass('show-signup')
+    $('.form-full-signup').addClass('show-full-signup')
+    $('.form-login').addClass('hide-login')
+    $('.form-recovery').addClass('hide-recovery')
+
+    setTimeout(function(){
+        $('.form-signup').removeClass('show-signup')
+        $('.form-full-signup').removeClass('show-full-signup')
+        $('.form-login').removeClass('hide-login')
+        $('.form-recovery').removeClass('hide-recovery')
+
+        $('.form-signup').css({
+            'background-color': '#fff',
+            'transform': 'translate(-35%, -20px) scaleY(1)'
+        })
+
+        $('.form-full-signup').css({
+            'background-color': '#fff',
+            'transform': 'translate(-35%, -20px) scaleY(1)'
+        })
+
+        $('.form-login').css({
+            'background-color': '#d7e7f1',
+            'transform': 'translate(40%, 10px) scaleY(1.2)'
+        })
+
+        $('.form-recovery').css({
+            'background-color': '#d7e7f1',
+            'transform': 'translate(40%, 10px) scaleY(1.8)'
+        })
+
+    }, 400)
+}
+
+// What happens when the "I forgot my password" button is pressed
+function handleForgotPw(){
+    // Button "I forgot my password"
+    let forgotButton = $("#forgot-button")
 
     // Adds event listener to the button
-    forgotButton.addEventListener('click', function(){
-        // Hides the login form
-        let loginForm = this.parentElement.parentElement
-        loginForm.style.display = "none"
-
+    forgotButton.on('click', function(){
+        let loginForm = $('.form-login')
         let wrapper = this.parentElement.parentElement.parentElement
+        let recoveryForm = $(".form-recovery")
 
-        // Shows the password recovery form
-        let recoveryForm = wrapper.querySelector(".form-recovery")
-        recoveryForm.style.display = "block"
+        // Reduces the login form's height (animation)
+        // Hides the login form and shows the password recovery one
+        loginForm.animate(
+            { height: 0.62*parseFloat(loginForm.css('height')) + 'px' }, 400, 
+            function() {
+                loginForm.hide()
+                recoveryForm.show()
+                
+                loginForm.css('height', 'auto')
+            }
+        )
 
         // Adds event listener to swap back
-        wrapper.querySelector(".switcher-login").removeEventListener('click', swapTab)
-        wrapper.querySelector(".switcher-login").addEventListener('click', function(){
-            if(wrapper.classList.contains("active") === true){
-                recoveryForm.style.display = "none"
-                loginForm.style.display = "block"
+        wrapper.querySelector('#recovery-back').addEventListener('click', function(){
+            if($(wrapper).hasClass("active")){
+                recoveryForm.animate(
+                    { height: 0.65*parseFloat(loginForm.css('height')) + 'px' }, 400,
+                    function() {
+                        recoveryForm.hide()
+                        loginForm.show()
+
+                        recoveryForm.css('height', 'auto')
+                    }
+                )
+
                 this.removeEventListener('click', arguments.callee)
             }
         })
-        wrapper.querySelector(".switcher-login").addEventListener('click', swapTab)
+    })
+}
+
+// From basic signup to full signup forms
+function handleFullSignup(){
+    let signupForm = $('.form-signup')
+    let wrapper = document.querySelector('.switcher-signup').parentElement
+    let fullSignupForm = $(".form-full-signup")
+
+    // Increases the login form's height (animation)
+    // Hides the first signup form and shows the full one
+    signupForm.animate(
+        { height: 1.3*parseFloat(signupForm.css('height')) + 'px' }, 400,
+        function(){
+            signupForm.hide()
+            fullSignupForm.show()
+            
+            signupForm.css('height', 'auto')
+        }
+    )
+
+    // Adds event listener to swap back
+    wrapper.querySelector('#signup-back').addEventListener('click', function(){
+        if($(wrapper).hasClass("active")){
+            fullSignupForm.animate(
+                { height: 0.7*parseFloat(signupForm.css('height')) + 'px' }, 400,
+                function() {
+                    fullSignupForm.hide()
+                    signupForm.show()
+
+                    fullSignupForm.css('height', 'auto')
+                }
+            )
+
+            this.removeEventListener('click', arguments.callee)
+        }
     })
 }
 
 // Validates the user data from the signup forms
 function validateSignup(forms){
     if(forms === 'signup'){
+        // Gets input data
         let pw = signup.pw
         let pwConf = signup.pwConfirmation
+
+        // Error message element
         let error = document.querySelector('#signup').querySelector('.error-message')
 
+        // Validates the password
         if(pw.value != pwConf.value){
             error.innerHTML = "Ambas as senhas devem ser idênticas."
             pwConf.focus()
-            return false
         }
 
         else{
             error.innerHTML = ""
-            return true
+            handleFullSignup()
         }
     }
 
     else if(forms === 'fullSignup'){
         // Function to validate birthdate formatted as "dd/mm/yyyy"
         function isValidDate(dateString){
-            // First check for the pattern
-            if(!/^\d{2}\/\d{2}\/\d{4}$/.test(dateString)){ return false }
-
-            // Parse the date parts to integers
+            // Parses the date parts to integers
             let parts = dateString.split("/")
             let day = parseInt(parts[0], 10)
             let month = parseInt(parts[1], 10)
@@ -94,20 +217,25 @@ function validateSignup(forms){
             return (day > 0 && day <= monthLength[month - 1])
         }
 
+        // Gets input data
         let name = fullSignup.fullName
         let birthday = fullSignup.birthday
         let cpf = fullSignup.CPF
         let phone = fullSignup.phoneNumber
-        let error = document.querySelector('#full-signup').querySelector('.error-message')
+
+        // Error message element
+        let error = $('#full-signup .error-message')
         
         name.value = name.value.title()
 
-        let dateList = birthday.value.split('/')
-        let day = dateList[0]
-        let month = dateList[1]
-        let year = dateList[2]
+        // Parses the date
+        let parts = birthday.value.split('/')
+        let day = parts[0]
+        let month = parts[1]
+        let year = parts[2]
         let birthdate = new Date(year, month-1, day)
-        
+
+        // Validates the birthdate
         if(!isValidDate(birthday.value)){
             let errorMsg = "A data de nascimento inserida é inválida"
 
@@ -127,30 +255,33 @@ function validateSignup(forms){
                 errorMsg = errorMsg + "."
             }
 
-            error.innerHTML = errorMsg
+            error.text(errorMsg)
             birthday.focus()
-            return false
         }
 
-        // // VALIDAR CPF
-        // else if(){
+        // Validates the CPF
+        else if(!(new CPF).validate(cpf.value)){
+            error.text("O CPF inserido é inválido.")
+            cpf.focus()
+        }
 
-        // }
+        // Validates the phonenumber
+        if(phone.value.substring(1,2) == '0' || phone.value.substring(1,3) == '10' || phone.value.substring(5,6) != '9'){
+            error.text("O número de celular inserido é inválido.")
+            phone.focus()
+        }
 
-        // // VALIDAR CELULAR
-        // else if(){
-
-        // }
-
+        // If no problem was found
         else{
-            return true
+            error.text("")
+            signup.submit()
+            fullSignup.submit()
         }
 
     }
 
     else{
-        alert("O nome do formulário foi passado incorretamente.")
-        return false
+        console.log("O nome do formulário foi passado incorretamente.")
     }
 }
 
@@ -215,32 +346,6 @@ function formatCPF(e){
     }
 }
 
-function formatRG(e){
-    // if a numeric key is numeric, allow it
-    if(e.key != ' ' && e.key <= 9 && e.key >= 0){
-        // insets dot and dash where it's needed
-        if(e.target.value.length === 1 || e.target.value.length === 5){ 
-            e.target.value = e.target.value + "." 
-        }
-
-        else if(e.target.value.length === 9){ 
-            e.target.value = e.target.value + "-" 
-        }
-    }
-
-    // excludes the dots and dashes when needed
-    else if(e.key === 'Backspace'){
-        if(e.target.value.length === 3 || e.target.value.length === 7 || e.target.value.length === 11){ 
-            e.target.value = e.target.value.substring(0, e.target.value.length - 1)
-        }
-    }
-
-    // disable alphabetic keys and the delete key
-    else if(e.key.length === 1 || e.key === 'Delete'){
-        return false
-    }
-}
-
 // Turns "00000000000" into "(00) 00000-0000" while the
 // user types (automatically inserts parenthesis and dash)
 // Allows only numeric keys to be entered
@@ -277,4 +382,3 @@ function formatPhoneNumber(e){
         return false
     }
 }
-
