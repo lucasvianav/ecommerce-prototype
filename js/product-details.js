1bc6bef8e69b7286ea0da8f28dbba9f9871cb2db
lucasvianav <oviana@outlook.com>
2020-11-05 17:52:02 -0300

    update product-details.js to jQuery

--- js/product-details.js
+++ js/product-details.js
@@ -1,36 +1,41 @@
+// Called when the html completes loading
+$(function(){
+  let slides = $('.large-photo')
+  let thumbs = $('.product-photo-thumb')
+  
+  // Hides all slides
+  slides.each(function(){ $(this).hide() })
+
+  // Deactivates all thumbnails and adds events
+  // listeners to them in order to control the slides
+  for(let i = 0; i < thumbs.length; i++){
+    $(thumbs[i]).removeClass('active')
+    $(thumbs[i]).bind('click', function(){ showSlides(i) }) // Binds evento to handler
+  }
+
+  // Shows the first slide and activates the corresponding thumbnail
+  $(slides[0]).show()
+  $(thumbs[0]).addClass('active')
+})
+
+// Slide of the index currently visible
 var currentSlide = 0;
 
 // Shows selected photo and hides all other
-function showSlides(n) {
-  let slides = document.getElementsByClassName("large-photo");
-  let thumbs = document.getElementsByClassName("product-photo-thumb");
+function showSlides(n){
+  let slides = $(".large-photo")
+  let thumbs = $(".product-photo-thumb")
 
-  if (n >= slides.length){ currentSlide = slides.length }
-  else if (n < 0){ currentSlide = 0 }
+  // Hides the current slide and deactivates it's thumbnail
+  $(slides[currentSlide]).hide()
+  $(thumbs[currentSlide]).removeClass('active')
+
+  // Sets new current slide index
+  if(n >= slides.length){ currentSlide = slides.length }
+  else if(n < 0){ currentSlide = 0 }
   else{ currentSlide = n }
 
-  for(let photo of slides){
-    photo.style.display = "none"
-  }
-
-  for(let thumbnail of thumbs){
-    thumbnail.classList.remove('active')
-  }
-
-  slides[currentSlide].style.display = "inline-block"
-  thumbs[currentSlide].classList.add('active')
-}
-
-// Adds events listeners to the thumbnails in order to control the slides
-// Called when the page completes loading
-function initSlides(){
-  showSlides(0)
-
-  let thumbs = document.getElementsByClassName("product-photo-thumb")
-
-  for(let i = 0; i < thumbs.length; i++){
-    thumbs[i].addEventListener('click', function(){
-      showSlides(i)
-    })
-  }
+  // Shows the current slide and activates it's thumbnail
+  $(slides[currentSlide]).show()
+  $(thumbs[currentSlide]).addClass('active')
 }
