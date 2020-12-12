29eafe07c2a2ace7216fc1dd1216fee162bead4e
Rafaela Bull <37142843+rafabull@users.noreply.github.com>
2020-12-11 23:41:48 -0300

    api CRUD de cupons pronta

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const productsRouter = require('./products')
-const accountRouter = require('./account')
-const authRouter = require('./auth')
-const cartRouter = require('./cart')
+const productsRouter = require('./products');
+const accountRouter = require('./account');
+const authRouter = require('./auth');
+const cartRouter = require('./cart');
+const couponsRouter = require('./coupons');
 
-router.use('/products', productsRouter)
-router.use('/accounts', accountRouter)
-router.use('/auth', authRouter)
-router.use('/cart', cartRouter)
+router.use('/products', productsRouter);
+router.use('/accounts', accountRouter);
+router.use('/auth', authRouter);
+router.use('/cart', cartRouter);
+router.use('/coupons', couponsRouter);
 
-module.exports = router
+module.exports = router;
